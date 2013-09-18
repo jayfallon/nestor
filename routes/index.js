df3907e5a902768036504fb69e5c95d9d41307ec
jayfallon <jayfallon@gmail.com>
2013-09-18 14:36:03 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+
+/*
+ * GET home page.
+ */
+
+module.exports = function(app){
+	app.get('/', function(req,res){
+		res.redirect('users/risk');
+	});
+
+//404
+// Handle 404
+  app.use(function(req, res) {
+      res.status(400);
+     res.render('404.jade', {title: '404: File Not Found'});
+  });
+
+  // Handle 500
+  app.use(function(error, req, res, next) {
+      res.status(500);
+     res.render('500.jade', {title:'500: Internal Server Error', error: error});
+  });
+};
